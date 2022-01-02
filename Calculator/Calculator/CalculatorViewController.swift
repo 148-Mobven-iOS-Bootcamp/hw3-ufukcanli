95c88a474962006557a7af2f6d9d0182511aab93
ufukcanli <iletisim@ufukcanli.com>
2022-01-02 21:14:20 +0300

    Add outlets

---
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by Ufuk Canlı on 2.01.2022.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var calculatorWorkingLabel: UILabel!
+    @IBOutlet weak var calculatorResultLabel: UILabel!
+    @IBOutlet var calculatorButtons: [UIButton]!
+    
+}
